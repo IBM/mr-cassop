74f9e91d0bbbbc0eea9959aef50708110c1216db
Anton Kuznietsov <xmananton@gmail.com>
2021-01-06 20:35:03 +0200

    Ci e2e general tests (#54)
    * added deploy to iks + ci refactoring
    
    * test workflow_run
    
    * test workflow_run
    
    * test workflow_run
    
    * test workflow_run
    
    * test workflow_run
    
    * test workflow_run
    
    * test workflow_run
    
    * test workflow_run
    
    * test workflow_run
    
    * update docusaurus and its deps
    
    * update docusaurus and its deps
    
    * update docusaurus and its deps #2
    
    * test
    
    * + lock
    
    * added missing deps
    
    * added lock
    
    * added missing lib
    
    * fix typo
    
    * test
    
    * test
    
    * test
    
    * refactoring
    
    * test
    
    * fix job names
    
    * test
    
    * test
    
    * test
    
    * test
    
    * fix typo
    
    * test limitation runs option for workflow
    
    * test when two parallel workflow builds
    
    * test with separate branches
    
    * test with separate branches
    
    * test when two parallel workflow builds
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * fix vars
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * fix var
    
    * fix var
    
    * test
    
    * test
    
    * test
    
    * test image deletion logic
    
    * test image deletion logic
    
    * test image deletion logic
    
    * test image deletion logic
    
    * test image deletion logic
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * speedup run
    
    * made docker proxy registry variable
    
    * switch release type to created
    
    * added conf and CR deploy step for ginkgo
    
    * save work
    
    * change
    
    * update modules versions
    
    * fix docker layers cache restoring
    
    * test hashing
    
    * update c* image
    
    * update node modules
    
    * changes to ci script
    
    * test
    
    * test
    
    * test reaper start issue
    
    * disable docker build layers caching
    
    * test
    
    * cahnges
    
    * add logs and timeout for reaper deployment
    
    * cahnges
    
    * ci improvements
    
    * changes
    
    * bump reaper version
    
    * update
    
    * test go e2e tests run in gh actions
    
    * test
    
    * added retry for cloud login
    
    * ci changes
    
    * test e2e in ci
    
    * made boilerplate code functions
    
    * fix ci
    
    * changes
    
    * add verbosity for go test and ginkgo
    
    * debug restConfig var issue
    
    * add go.sum
    
    * change name
    
    * save work
    
    * merge go e2e test files
    
    * fix var reference
    
    * test
    
    * add workaround of not visible var pointer issue
    
    * update ginkgo lib version and github.com/google/go-cmp
    
    * test
    
    * fix linter test
    
    * update gocql module
    
    * add cql test query
    
    * update gocql
    
    * fix linting issues + improvements
    
    * wrap up with retry block another cli commands which can fail
    
    * add missing go.sum file
    
    * add reaper repair job completion test
    
    * added reaper reschedules test
    
    * fix assertions
    
    * print pod statuses if iteration failed
    
    * rename workflow file
    
    * cleanup
    
    * added pod logs output if fail
    
    * resolve addressed pr comments
    
    * fix lint error
    
    * remove setting limits for C* pods which produce OOMKilled error
    
    * increase number line to tail
    
    * remove alias
    
    * remove redundant args and output
    
    * fail if environment variables are not set
    
    * return false if error
    
    * moved part of tasks from todo to github issue
    
    Co-authored-by: Anton Kuznietsov <anton.kuznietsov@ibm.com>

---
+++ tests/e2e/utils_test.go
@@ -0,0 +1,190 @@
+package e2e
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	v1alpha1 "github.com/ibm/cassandra-operator/api/v1alpha1"
+	"io"
+	"io/ioutil"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/portforward"
+	"k8s.io/client-go/transport/spdy"
+	"net/http"
+	"net/url"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"strings"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var (
+	httpClient = &http.Client{
+		Timeout: time.Second * 5,
+	}
+)
+
+func waitForPodsReadiness(namespace string, labels map[string]string) {
+	Eventually(func() bool {
+		podList := &corev1.PodList{}
+		err = restClient.List(context.Background(), podList, client.InNamespace(namespace), client.MatchingLabels(labels))
+		Expect(err).To(Succeed())
+
+		if len(podList.Items) == 0 {
+			return false
+		}
+
+		for _, pod := range podList.Items {
+			for _, container := range pod.Status.ContainerStatuses {
+				if !container.Ready {
+					return false
+				}
+			}
+		}
+		return true
+	}, time.Minute*10, time.Second*2).Should(BeTrue(), "Pods should become ready: ", labels[v1alpha1.CassandraClusterComponent])
+}
+
+func waitPodsTermination(namespace string, labels map[string]string) {
+	Eventually(func() bool {
+		podList := &corev1.PodList{}
+		err = restClient.List(context.Background(), podList, client.InNamespace(namespace), client.MatchingLabels(labels))
+		Expect(err).To(Succeed())
+
+		return len(podList.Items) == 0 //return true if expression is true
+	}, time.Minute*10, time.Second*5).Should(BeTrue())
+}
+
+func waitForCassandraClusterSchemaDeletion(namespace string, release string) {
+	Eventually(func() metav1.StatusReason {
+		err = restClient.Get(context.Background(), types.NamespacedName{
+			Namespace: namespace,
+			Name:      release,
+		}, &v1alpha1.CassandraCluster{})
+
+		if err != nil {
+			if statusErr, ok := err.(*errors.StatusError); ok {
+				return statusErr.ErrStatus.Reason
+			}
+		}
+
+		return "Incorrect. Schema still exit."
+	}, time.Second*5).Should(Equal(metav1.StatusReasonNotFound), "Cassandra cluster CassandraCluster schema should be deleted")
+}
+
+func portForwardPod(namespace string, labels map[string]string, portMappings []string, restClientConfig *rest.Config) *portforward.PortForwarder {
+	rt, upgrader, err := spdy.RoundTripperFor(restClientConfig)
+	Expect(err).To(Succeed())
+
+	podList := &corev1.PodList{}
+	err = restClient.List(context.Background(), podList, client.InNamespace(namespace), client.MatchingLabels(labels))
+	Expect(err).To(Succeed())
+	Expect(len(podList.Items)).ToNot(BeEquivalentTo(0), "pods not found for port forwarding")
+
+	pod := podList.Items[0]
+	fmt.Printf("pod.Name: %v", pod.Name)
+	path := fmt.Sprintf("/api/v1/namespaces/%s/pods/%s/portforward", pod.Namespace, pod.Name)
+	hostIP := strings.TrimLeft(restClientConfig.Host, "htps:/")
+	serverURL := url.URL{Scheme: "https", Path: path, Host: hostIP}
+
+	dialer := spdy.NewDialer(upgrader, &http.Client{Transport: rt}, http.MethodPost, &serverURL)
+
+	stopChan, readyChan := make(chan struct{}, 1), make(chan struct{}, 1)
+	out, errOut := new(bytes.Buffer), new(bytes.Buffer)
+	pf, err := portforward.New(dialer, portMappings, stopChan, readyChan, out, errOut)
+	Expect(err).To(Succeed())
+
+	go func() {
+		defer GinkgoRecover()
+		for range readyChan {
+		}
+
+		if len(errOut.String()) != 0 {
+			Fail(fmt.Sprintf("Port forwarding failed: %s", errOut.String()))
+		} else if len(out.String()) != 0 {
+			_, err := fmt.Fprintf(GinkgoWriter, "Message from port forwarder: %s", out.String())
+			Expect(err).To(Succeed())
+		}
+	}()
+
+	go func() {
+		defer GinkgoRecover()
+		err = pf.ForwardPorts() // this will block until stopped
+		Expect(err).To(Succeed())
+	}()
+
+	for range readyChan {
+	} // wait till ready
+
+	return pf
+}
+
+func doHTTPRequest(method string, url string) (error, []byte, int) {
+
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return err, nil, 0
+	}
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return err, nil, 0
+	}
+	defer resp.Body.Close()
+
+	// Parse response
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err, nil, 0
+	}
+
+	return err, body, resp.StatusCode
+}
+
+func findFirstRepairMapByKV(repair []map[string]interface{}, k string, v string) map[string]interface{} {
+	for _, iterMap := range repair {
+		// This works only if the key has value of type []interface(array in term of json) and we take only the first its element. That's why you can't specify more than 1 table per reaper repair in e2e tests.
+		if i := iterMap[k].([]interface{})[0]; i == v { // Extract string value from slice of interfaces in unstructured json
+			return iterMap
+		}
+	}
+	return nil
+}
+
+func testReaperRescheduleTime(reqTime time.Time, respTime time.Time, nowTime time.Time) {
+	Expect(respTime.Weekday()).To(BeEquivalentTo(reqTime.Weekday()), "Week day should match.")
+
+	if respTime.Year() == reqTime.Year() { // There is a case when schedules set with previous year
+		Expect(respTime.YearDay()).To(BeNumerically(">=", reqTime.YearDay()), "Year day should be equal or greater than scheduled.")
+		Expect(respTime.YearDay()).To(BeNumerically(">=", nowTime.YearDay()), "Year day should be equal or greater than current.")
+	}
+
+	Expect(respTime.Unix()).To(BeNumerically(">=", respTime.Unix()), "Unix timestamp should be greater or equal scheduled.")
+	Expect(respTime.Unix()).To(BeNumerically(">=", nowTime.Unix()), "Unix timestamp should be greater or equal current.")
+	Expect(respTime.YearDay()-reqTime.YearDay()).To(BeNumerically("<=", 7), "Year day difference should be less or equal 7.")
+}
+
+func getPodLogs(pod corev1.Pod, kubeClient *kubernetes.Clientset, podLogOpts corev1.PodLogOptions) (error, string) {
+	req := kubeClient.CoreV1().Pods(pod.Namespace).GetLogs(pod.Name, &podLogOpts)
+	podLogs, err := req.Stream(context.Background())
+	if err != nil {
+		return err, ""
+	}
+	defer podLogs.Close()
+
+	buff := new(bytes.Buffer)
+	_, err = io.Copy(buff, podLogs)
+	if err != nil {
+		return err, ""
+	}
+	str := buff.String()
+
+	return err, str
+}
